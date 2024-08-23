e2267c0cacc4eee85a40278295421b1a7b7c80f3
Palina <polina.sofina.20@mail.ru>
2024-08-23 14:56:14 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,12 +1,14 @@
-def send_email(massage, recipient, sender = 'university.help@mail.com'):
-    if ('@' and ('.com' or '.ru' or '.net')) not in recipient and ('@' and ('.com' or '.ru' or '.net')) not in sender:
-        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+def send_email(massage, recipient, *, sender = 'university.help@mail.com'):
+    if not recipient.__contains__('@') and not recipient.endswith('.com' or '.ru' or '.net'):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif not sender.__contains__('@') and not sender.endswith('.com' or '.ru' or '.net'):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
     elif sender == 'university.help@mail.com':
-        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
